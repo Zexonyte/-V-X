ef46e554a8fd32d7ac909b17552e7f1797aefea3
Zexonyte <35183342+Zexonyte@users.noreply.github.com>
2018-01-09 19:16:30 -0500

    Add files via upload

--- !V'X.py
+++ !V'X.py
@@ -1,10 +1,12 @@
 import time
-def interpret(prgm):
+def interpret(prgm, args*):
     s = time.time()
     if prgm[-3:] != ".vx": prgm += ".vx"
     try: p = open(prgm).read()
     except FileNotFoundError: return "File not found"
+    for a in args: int(a)
     x = 0
+    a = 0
     l = [0,0,0,0]
     lp = 0
     e = 0
@@ -24,8 +26,8 @@ def interpret(prgm):
                 elif p[x] == ">": x += 1
             else: x += 4
         elif p[x] == "!":
-            if mp == 255: m[0] = m[mp]
-            else: m[mp+1] = m[mp]
+            if a == len(args): continue
+            else: m[next_0_reg(m)] = args[a]; a += 1
         elif p[x] == ">": mp = convert(p[x+1:x+5])
         elif p[x] == "&":
             if mp == 255: print(chr(m[mp]),end='',flush=True)
@@ -37,19 +39,22 @@ def interpret(prgm):
             m[mp] -= 1
             if m[mp] < 0: m[mp] = 255
         x += 1
-    del x, l, lp, e, m, mp; print(); return "Finished interpreting program successfully\nTime: %s"%(time.time()-s)
+    del x, l, lp, e, m, mp, a; print(); return "Finished interpreting program successfully\nTime: %s"%(time.time()-s)
 def convert(b):
     n = ["0","0","0","0","0","0","0","0"]
     t = {".":[4,5,6,7],";":[0,1,2,3],":":[(4,0),(5,1),(6,2),(7,3)]}
-    for x in range(0, 4):
+    for x in range(4):
         if b[x] == "|": continue
         elif b[x] == ".": n[t["."][x]] = "1"
         elif b[x] == ";": n[t[";"][x]] = "1"
         elif b[x] == ":": n[t[":"][x][0]] = "1"; n[t[":"][x][1]] = "1"
     return int("".join(n),2)
 a = ""
+def next_0_reg(l):
+    for n in range(256):
+        if l[n] == 0: return n
+    return 0
 while True:
-    a = input("!V'X> ")
-    if a[0] == ".":
-        if a == ".quit": break
-    else: print(interpret(a))
+    a = input("!V'X> ").split()
+    if a[0] == ".quit": break
+    else: print(interpret(a[0], *a[1:]))
